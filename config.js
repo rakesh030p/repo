3d2615e8b1f0851ec2d8da67e5cd07b47544e729
rakesh030p <93330376+rakesh030p@users.noreply.github.com>
2023-10-19 15:23:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "My happy crush HaHaHaHA",
+    introTitle: 'Hello Erika',
+    introDesc: `The earth is so big,
+    but why do you keep going the wrong way? 
+    You got lost in my heart all alone.`,
+    btnIntro: '^^HiHi^^',
+    title: 'You are my happy crush HAHAHAHA🥰',
+    desc: ' ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
